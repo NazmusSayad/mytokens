5413abe41182f89189abf69fa99aed6d968b9eb4
Nazmus Sayad <87106526+NazmusSayad@users.noreply.github.com>
2026-05-04 12:38:00 +0600

    feat: enhance renderScreen function to log terminal dimensions

--- src/render/render-screen.ts
+++ src/render/render-screen.ts
@@ -8,4 +8,9 @@ export type RenderDataItem = {
 export async function renderScreen(
   data: RenderDataItem[],
   showBy: 'day' | 'week' | 'month' | 'year' = 'day'
-) {}
+) {
+  const width = process.stdout.columns ?? 80
+  const height = process.stdout.rows ?? 40
+
+  console.log(width, height)
+}
