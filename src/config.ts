b618a989b81566e63277cc77042da95ae92c4128
Nazmus Sayad <87106526+NazmusSayad@users.noreply.github.com>
2026-05-05 18:41:16 +0600

    fix: update CACHE_VALIDITY_DURATION to 1 hour in config.ts

--- src/config.ts
+++ src/config.ts
@@ -2,7 +2,7 @@ import ms from 'ms'
 import os from 'os'
 import path from 'path'
 
-export const CACHE_VALIDITY_DURATION = ms('12h')
+export const CACHE_VALIDITY_DURATION = ms('1h')
 
 export const OPENUSAGE_PATH = path.join(os.homedir(), '.openusage')
 export const OPENUSAGE_CACHE_PATH = path.join(OPENUSAGE_PATH, 'cache')
